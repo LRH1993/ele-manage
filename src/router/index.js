87bb6ea5a1e3a8d3fcd84f9799565675f958a306
LRH1993 <799386120@qq.com>
2018-01-12 11:18:57 +0800

    login路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
+
+import login from '../page/login.vue'
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: login
     }
   ]
 })
