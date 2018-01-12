3a28c7c6aab6c74e6065ca12901b3ad2bac89c51
LRH1993 <799386120@qq.com>
2018-01-12 10:55:00 +0800

    网络请求配置

---
+++ src/config/env.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * baseUrl: 域名地址
+ * routerMode: 路由模式
+ * baseImgPath: 图片存放地址
+ *
+ */
+let baseUrl = 'http://cangdu.org:8001';
+let routerMode = 'history';
+let baseImgPath = 'http://cangdu.org:8001/img/';
+
+
+export {
+  baseUrl,
+  routerMode,
+  baseImgPath
+}
